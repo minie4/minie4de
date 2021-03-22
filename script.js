67515d3c907fff8c890d3c1569d924bfd053f83f
minie4 <minie4@gmx.de>
2021-03-22 23:07:43 +0100

    Built a responsive navigatiom bar and started building the welcome-block

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMobileNavbar() {
+    var x = document.querySelector(".navbar");
+    if (x.className === "navbar") {
+        x.className += " mobile";
+    } else {
+        x.className = "navbar";
+    }
+}
