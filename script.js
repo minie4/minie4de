189ebd7a4dd6d10cbc465aef218522becb6b9769
minie4 <minie4@gmx.de>
2022-01-17 00:50:15 +0100

    🎨 Convert to indentation to 2 spaces

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function toggleMobileNavbar() {
-    var x = document.querySelector(".navbar");
-    if (x.className === "navbar") {
-        x.className += " mobile";
-    } else {
-        x.className = "navbar";
-    }
+  var x = document.querySelector(".navbar");
+  if (x.className === "navbar") {
+    x.className += " mobile";
+  } else {
+    x.className = "navbar";
+  }
 }
